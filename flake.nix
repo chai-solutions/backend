dd4b4859faedd3e0057f16c3ab48f056710c57f3
Varun Narravula <varun@snare.dev>
2024-09-09 14:28:42 -0700

    feat: package with nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "Description for the project";
 
@@ -15,11 +14,26 @@
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
 
       perSystem = {pkgs, ...}: {
+        packages.default = pkgs.buildGoModule {
+          pname = "chai-solutions-backend";
+          version = "0.0.1";
+          src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+
+          vendorHash = "sha256-NA5fgx+uzQQKmAuJwGIBtTGU9QcrM54BiGQ+kz03pYk=";
+
+          meta = with pkgs.lib; {
+            description = "Chai Solutions backend";
+            homepage = "https://chai-solutions.org";
+            license = licenses.gpl3Only;
+            maintainers = with maintainers; [water-sucks];
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           name = "chai-solutions-backend";
           buildInputs = with pkgs; [
-              go
-              gotools
+            go
+            gotools
           ];
         };
       };
