7ec3f894f31ff31978e1ecfca16249fe956e18e3
Vignesh <realvignesh@gmail.com>
2024-09-11 14:03:29 -0700

    Removed dup files

--- db/account.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "accounts" (
-  "id" bigserial PRIMARY KEY,
-  "owner" varchar NOT NULL,
-  "balance" bigint NOT NULL,
-  "currency" varchar NOT NULL,
-  "created_at" timestamp DEFAULT (now())
-);
