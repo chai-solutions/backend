0e818a4de9a3ea70056416e29ae961ac6daa09f6
Vignesh <realvignesh@gmail.com>
2024-09-23 21:09:56 -0700

    feat: embedding migrations to binary and switched to dbmate library

--- database/connection.go
+++ database/connection.go
@@ -3,10 +3,17 @@ package database
 import (
 	"chai/database/sqlc"
 	"context"
+	"embed"
+	"net/url"
 
+	"github.com/amacneil/dbmate/v2/pkg/dbmate"
+	_ "github.com/amacneil/dbmate/v2/pkg/driver/postgres"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog/log"
 )
+
+//go:embed migrations/*.sql
+var fs embed.FS
 
 func NewPool(url string) (*pgxpool.Pool, *sqlc.Queries) {
 	pool, err := pgxpool.New(context.Background(), url)
@@ -16,4 +23,21 @@ func NewPool(url string) (*pgxpool.Pool, *sqlc.Queries) {
 	queries := sqlc.New(pool)
 
 	return pool, queries
+}
+
+func RunMigrations(databaseURL string) {
+	u, err := url.Parse(databaseURL)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Invalid database URL")
+	}
+
+	db := dbmate.New(u)
+	db.FS = fs // Set the embedded filesystem for migrations
+	db.MigrationsDir = []string{"migrations"}
+	db.AutoDumpSchema = false
+
+	err = db.CreateAndMigrate()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error migrating")
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module chai
 go 1.22.6
 
 require (
+	github.com/amacneil/dbmate/v2 v2.21.0
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/jackc/pgx/v5 v5.7.0
 	github.com/rs/zerolog v1.33.0
@@ -12,10 +13,11 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
