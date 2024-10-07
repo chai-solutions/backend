2ffc8c6f776ddbfb59bb173b72456f31ccdf02c0
Varun Narravula <varun@snare.dev>
2024-10-07 13:07:15 -0700

    feat: add godotenv for automatic env var loading during dev

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
           version = "0.0.1";
           src = pkgs.nix-gitignore.gitignoreSource [] ./.;
 
-          vendorHash = "sha256-s8tLJ0ynVOYKYmlCXnttd7kpRugxYFTwczAH9Fr4dk0";
+          vendorHash = "sha256-IWoL6Xt1Z+zYgK7Hq90VG26/J5oXQsxdNclvxUnq2yI=";
 
           postInstall = ''
             mv $out/bin/cmd $out/bin/chaid
